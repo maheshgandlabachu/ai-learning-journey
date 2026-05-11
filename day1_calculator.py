8cd96ffa641ef3c98fa0095f090158dbaf3cbb62
Mahesh Gandla <mgandlabachu@gmail.com>
2026-05-10 22:03:04 -0400

    implemented conditional logics in the code

--- day1_calculator.py
+++ day1_calculator.py
@@ -3,11 +3,22 @@
 num1 = int(input("Enter the num1:"))
 num2 = int(input("Enter the num2:"))
 
+choice = int(input("Enter your choice between 1 and 4: "))
+if choice == 1:
+    print("Result:", num1 + num2)
+elif choice == 2:
+    print("Result:", num1 - num2)
+elif choice == 3:
+    print("Result:", num1 * num2)
+elif choice == 4:
+    print("Result:", num1 / num2)
+else:
+    print("Invalid choice")
+                    
+
 print("Addition", num1 + num2)
 print("Subraction", num1 - num2)
 print("Multipication", num1 * num2)
 print("division", num1 / num2)
 
 print("Caluculator is finished  ")
-
-
