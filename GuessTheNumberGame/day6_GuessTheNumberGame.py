2998cb342473781768428c0f775ed8f68cb5d3f6
Mahesh Gandla <mgandlabachu@gmail.com>
2026-05-13 11:45:04 -0400

    Added nested loops and implemented replay logic

--- GuessTheNumberGame/day6_GuessTheNumberGame.py
+++ GuessTheNumberGame/day6_GuessTheNumberGame.py
@@ -1,13 +1,20 @@
 #building a number guess project
+# added nested loops and replay logic (05-13-2025)
 import random
 secret_number = random.randint(1,101)
+attempt = 0
 while True:
-    user_guess = int(input("what is your guess? :  "))
-    if user_guess > secret_number:
-        print("guess is too high")
-    elif user_guess < secret_number:
-        print("guess is too low")
-    else :
-        print(f"Hurray you did it, your guess is correct and guess number is : {user_guess}")
+    while True:
+        attempt += 1
+        user_guess = int(input("what is your guess? :  "))
+        if user_guess > secret_number:
+            print("guess is too high")
+        elif user_guess < secret_number:
+            print("guess is too low")
+        else :
+            print(f"Hurray you did it, your guess is correct and guess number is : {user_guess}")
+            break
+    replay = input("Do you want to play again? Yes/No: ")
+    if replay.lower() == "no":
         break
-    
+print(f"You have taken {attempt} attempts to guess the secret number :-) Good job!  ")
